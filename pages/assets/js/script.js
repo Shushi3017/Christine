732b66d68903b834de9071f353a53bb083c035cc
Shushi3017 <vdtibay@fit.edu.ph>
2026-02-15 05:29:04 +0800

    Add initial version of practice.html for Christine's Love Story with audio and interactive elements

--- pages/assets/js/script.js
+++ pages/assets/js/script.js
@@ -1,23 +1,13 @@
-onload = () => {
-  document.body.classList.remove("container");
-};
-// Function to start music
-function playMusic() {
-    const music = document.getElementById('bg-audio');
-    music.play().then(() => {
-        // Music started successfully
-        console.log("Music playing");
-        // Remove listeners once it starts playing
-        window.removeEventListener('click', playMusic);
-        window.removeEventListener('scroll', playMusic);
-        window.removeEventListener('touchstart', playMusic);
-    }).catch((error) => {
-        // Autoplay was prevented
-        console.log("Playback failed:", error);
-    });
-}
 
-// Listen for any user interaction to trigger the music
-window.addEventListener('click', playMusic);
-window.addEventListener('scroll', playMusic);
-window.addEventListener('touchstart', playMusic);
+onload = () => {
+    const c = setTimeout(() => {
+      document.body.classList.remove("not-loaded");
+      clearTimeout(c);
+    }, 1000);
+
+
+  var audio = document.getElementById("audio");
+  audio.play().catch(error => {
+      console.log("Autoplay was prevented:", error);
+  });
+};
